0b61d9d856cd991d91e4fd0ee1a06837e4781dcb
LeslieAine <aineleslie@gmail.com>
2023-02-15 08:24:20 +0300

    change api to fix error

--- src/modules/getMovies.js
+++ src/modules/getMovies.js
@@ -1,43 +1,9 @@
-const url = 'https://www.tvmaze.com/shows';
-// const moviesEndpoint = 'shows';
-// const moviesRange = '2';
-
+const url = 'https://api.tvmaze.com/shows';
 
 const getMovies = async () => {
-  const response = await fetch(`${url}`, {
-    method: 'GET',
-    mode: 'no-cors',
-  });
+  const response = await fetch(`${url}`)  
   const data = await response.json();
-  console.log(data);
-  return data;
+  return data.slice(0,20);
 };
 
-// export default getMovies;
-
-// const getMovies = async () => {
-//     const response = await fetch(`${url}${moviesEndpoint}${moviesRange}`, {
-//       method: 'GET',
-//       mode: 'no-cors',
-//     });
-//     const data = await response.json();
-//     console.log(data);
-//     return data;
-//   };
-
-//   export default getMovies;
-// const url = 'http://localhost:8081/proxy?url=https://api.tvmaze.com/shows/2';
-// const getMovies = async () => {
-//     try {
-//       const response = await fetch(`${url}`);
-//       if (!response.ok) {
-//         throw new Error('Network response was not ok');
-//       }
-//       const data = await response.json();
-//       return data;
-//     } catch (error) {
-//       console.error('Error:', error);
-//     }
-//   };
-  
-//   export default getMovies;
+export default getMovies;
