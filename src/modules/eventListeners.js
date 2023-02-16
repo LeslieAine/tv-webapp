10f7e904bb9722911f1fda0f6761270d5591de75
TayyabKM <109720981+TayyabKM@users.noreply.github.com>
2023-02-16 14:30:13 +0100

    fix files

--- src/modules/eventListeners.js
+++ src/modules/eventListeners.js
@@ -6,49 +6,12 @@ const pop = document.querySelector('.pop');
 const body = document.querySelector('body');
 
 
-document.addEventListener('DOMContentLoaded', async () => {
-  //   addLikes();
-
-
-
 export const addCommentButtonsLister = async () => {
   const movies = await getMovies();
   const btns = document.querySelectorAll('.comment-btn');
   popup(btns, movies);
 };
 
-// movieSection.addEventListener('click', async (e) => {
-//     if (e.target.classList.contains('empty')) {
-//         alert(e.target)
-//         const movieId = e.target.closest('.movies').querySelector('.movie-id').textContent;
-//         document.querySelector('.empty').classList.add('hidden');
-//         document.querySelector('.filled').classList.remove('hidden');
-//         await addLike(movieId);
-//         addLikes();
-//     }
-// });
-
-// export const addListeners = () => {
-//   const emptyLikes = document.querySelectorAll(".empty");
-//   const filledLikes = document.querySelectorAll(".filled");
-//   Array.from(emptyLikes).forEach((likeImg) => {
-//     likeImg.addEventListener("click", async (e) => {
-//       e.target.classList.add("hidden");
-//       e.target.nextElementSibling.classList.remove("hidden");
-//       const movieId = e.target
-//         .closest(".list")
-//         .querySelector(".movie-id").textContent;
-//       await addLike(movieId);
-//     });
-//   });
-
-//   Array.from(filledLikes).forEach((filledImg) => {
-//     filledImg.addEventListener("click", (e) => {
-//       e.target.classList.add("hidden");
-//       e.target.previousElementSibling.classList.remove("hidden");
-//     });
-//   });
-// };
 
 pop.addEventListener('keydown', (e) => {
   if (e.key === 'Escape') {
