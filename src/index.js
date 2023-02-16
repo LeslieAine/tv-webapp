a5a47797efff6c47639d44f281eb30fb40d7ab2f
LeslieAine <aineleslie@gmail.com>
2023-02-16 18:29:37 +0300

    Add movies counter

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './style.css';
 import './modules/renderMovies.js';
 import displayMovies from './modules/displayMovies.js';
 import addListeners from './modules/eventListeners.js';
+import './modules/counters/moviesCounter.js';
 
 const displayUi = async () => {
   await displayMovies();
--- src/modules/eventListeners.js
+++ src/modules/eventListeners.js
@@ -1,8 +1,13 @@
 import { addLike } from './involvementAPI/getLikes.js';
 import addLikes from './involvementAPI/addLikes.js';
+import getMovies from './getMovies.js';
+import moviesCounter from './counters/moviesCounter.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
   addLikes();
+  /* eslint-disable no-unused-vars */
+  const movies = await getMovies();
+  document.querySelector('.main-movies-counter').textContent = `(${moviesCounter()})`;
 });
 
 const addListeners = () => {
