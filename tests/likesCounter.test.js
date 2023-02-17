f936d4dc705a47a331a2c66d15ed34856c0e78e7
LeslieAine <aineleslie@gmail.com>
2023-02-17 15:00:53 +0300

    likes counter test

---
+++ tests/likesCounter.test.js
@@ -0,0 +1,17 @@
+const likesCounter = document.querySelector('.likes');
+
+describe('likesCounter', () => {
+    beforeEach(() => {
+      document.body.innerHTML = '<p class="likes like">0</p>';
+    });
+  
+    test('should return the correct number of likes', () => {
+      expect(likesCounter()).toBe(1);
+    });
+  
+    test('should return 0 if there are no likes', () => {
+      document.body.innerHTML = '';
+      expect(likesCounter()).toBe(0);
+    });
+});
+  
