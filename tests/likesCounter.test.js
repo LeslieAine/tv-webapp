f62b2b7a639b2f1033af0236239a864eb5136fa6
LeslieAine <aineleslie@gmail.com>
2023-02-17 15:23:04 +0300

    fix linters

--- tests/likesCounter.test.js
+++ tests/likesCounter.test.js
@@ -1,17 +1,16 @@
 const likesCounter = document.querySelector('.likes');
 
 describe('likesCounter', () => {
-    beforeEach(() => {
-      document.body.innerHTML = '<p class="likes like">0</p>';
-    });
-  
-    test('should return the correct number of likes', () => {
-      expect(likesCounter()).toBe(1);
-    });
-  
-    test('should return 0 if there are no likes', () => {
-      document.body.innerHTML = '';
-      expect(likesCounter()).toBe(0);
-    });
+  beforeEach(() => {
+    document.body.innerHTML = '<p class="likes like">0</p>';
+  });
+
+  test('should return the correct number of likes', () => {
+    expect(likesCounter()).toBe(1);
+  });
+
+  test('should return 0 if there are no likes', () => {
+    document.body.innerHTML = '';
+    expect(likesCounter()).toBe(0);
+  });
 });
-  
